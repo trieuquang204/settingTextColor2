aa98da0c1eef991a5154827a3a91cbb55a023f67
trieuquang <trieuquang071997@gmail.com>
2021-03-10 14:16:20 +0700

    13 chuc nang thay doi mau sac

--- src/App.js
+++ src/App.js
@@ -6,18 +6,36 @@ import Reset from "./components/Reset";
 import Result from "./components/Result";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "red",
+      fontSize: 15,
+    };
+    this.onSetColor = this.onSetColor.bind(this);
+  }
+
+  onSetColor(param) {
+    this.setState({
+      color: param
+    })
+  }
+
   render() {
     return (
       <div className="container mt-50">
         <div className="row">
-          <ColorPicker />
+          <ColorPicker
+            color={this.state.color}
+            onReceiveColor={this.onSetColor}
+          />
 
           <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
             <SizeSetting />
 
             <Reset />
           </div>
-          <Result />
+          <Result color={this.state.color} />
         </div>
       </div>
     );
--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ColorPicker extends Component {
-    render() {
-        return (
-            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                <div className="panel panel-primary">
-                    <div className="panel-heading">
-                        <h3 className="panel-title">Color Picker</h3>
-                    </div>
-                    <div className="panel-body">
-                        <span></span>
-                        <hr />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: ["red", "blue", "green", "#bbb", "#000", "#ccc"],
+    };
+  }
+
+  showColor(color) {
+    return {
+      backgroundColor: color,
+    };
+  }
+
+  setActiveColor(color) {
+    this.props.onReceiveColor(color);
+  }
+
+  render() {
+    var elmColors = this.state.colors.map((color, index) => {
+      return (
+        <span
+          key={index}
+          style={this.showColor(color)}
+          className={this.props.color === color ? "active" : ""}
+          onClick={() => this.setActiveColor(color)}
+        ></span>
+      );
+    });
+    return (
+      <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        <div className="panel panel-primary">
+          <div className="panel-heading">
+            <h3 className="panel-title">Color Picker</h3>
+          </div>
+          <div className="panel-body">
+            {elmColors}
+            <hr />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ColorPicker;
